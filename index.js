7e714f6913c130ab48759c5316e6e83a2dc9e1ed
James McKay <jamestm97@gmail.com>
2017-09-19 23:42:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = " " + specialGuest + "!";
+var specialGuest = " " + specialGuest + "!";
 
 var topic = "space";
 
